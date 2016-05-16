cc0980bd66b7be221ac7ad283b00ca313c7a0d8c
Yvette <libra1014tyb@163.com>
2016-05-16 14:59:11 +0800

    complete

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,18 @@
 function calculate_median(arr) {
-  //请勿改动函数名
+	var midNum;
+	var len = arr.length;
+	arr.sort(compare);
+
+	if (arr.length%2 == 0) {
+		midNum = (arr[len/2-1] + arr[len/2])/2;
+	}else {
+		midNum = arr[(len-1)/2];
+	}
+
+	return midNum;
 }
 
+function compare(value1, value2){
+	return value1 - value2;
+}
 module.exports = calculate_median;
